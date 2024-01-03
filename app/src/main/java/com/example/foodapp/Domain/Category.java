3bb5030178b2c796837beadd2d10571ca4621021
Phạm Minh Tâm <107335521+MinhTamjs@users.noreply.github.com>
2024-01-03 22:27:51 +0700

    Add some model
    A some model in database but is not complete yet

---
+++ app/src/main/java/com/example/foodapp/Domain/Category.java
@@ -0,0 +1,37 @@
+package com.example.foodapp.Domain;
+
+public class Category {
+    private int id;
+    private String ImagePath;
+    private String Name;
+
+    public Category(int id, String imagePath, String name) {
+        this.id = id;
+        ImagePath = imagePath;
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
