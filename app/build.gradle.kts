85d5d01bf4f8df3bb6992943d3d11cfb6727f96b
Phạm Minh Tâm <107335521+MinhTamjs@users.noreply.github.com>
2024-01-09 15:50:06 +0700

    Final upload before testing new API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation(fileTree(mapOf("dir" to "E:\\Mobile Projects\\ZalopayTest", "include" to listOf("*.aar", "*.jar"))))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
